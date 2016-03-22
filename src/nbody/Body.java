5b7351efae4e03b71c8fa457b8ed2907fa16dfc5
Jordan <Jordan@10.200.6.210>
2016-03-22 10:24:37 -0500

    The tails are finally working!!! Credit goes to Salem, Paul, and Tanner for the help

--- src/nbody/Body.java
+++ src/nbody/Body.java
@@ -4,48 +4,63 @@ import edu.princeton.cs.StdDraw;
 import java.awt.Color;
 import java.util.ArrayList;
 
-/******************************************************************************
- *  Compilation:  javac Body.java
- *  Execution:    java Body
- *  Dependencies: Vector.java StdDraw.java
+/**
+ * ****************************************************************************
+ * Compilation: javac Body.java Execution: java Body Dependencies: Vector.java
+ * StdDraw.java
  *
- *  Implementation of a 2D Body with a position, velocity and mass.
+ * Implementation of a 2D Body with a position, velocity and mass.
  *
  *
- ******************************************************************************/
-
+ *****************************************************************************
+ */
 public class Body {
+
     private Vector r;      // position
     private Vector v;      // velocity
     private final double mass;   // mass
     private final Color color; //try to choose color
     private final double size;
-    private ArrayList tail;
+    private ArrayList<double[]> tail = new ArrayList<>();
 
     /**
      *
      * @param r position
      * @param v velocity
      * @param mass mass of bodies
-     * @param size  changes the size of the dots in input file
+     * @param size changes the size of the dots in input file
      * @param color this changes the colors in RGB format
      */
     public Body(Vector r, Vector v, double mass, double size, Color color) {
         this.r = r;
         this.v = v;
         this.mass = mass;
-        this.size = size*0.05;
+        this.size = size * 0.05;
         this.color = color;
-        
+
     } // Body( Vector, Vector, double )
 
-
     public void move(Vector f, double dt) {
-        Vector a = f.times(1/mass);
+        Vector a = f.times(1 / mass);
         v = v.plus(a.times(dt));
         r = r.plus(v.times(dt));
+        this.update(r);
     } // move( Vector, double )
 
+    public void update(Vector r) {
+        double[] position = {r.cartesian(0), r.cartesian(1)};
+        tail.add(0, position);
+        if (tail.size() > 50 ) {
+            tail.remove(tail.size() - 1);
+        }
+        /*for (int i = 0; i < tail.size() - 1; i++) {
+            double[] extra = tail.get(i + 1);
+            tail.set(i, extra);
+        }//for loop*/
+
+        /*double[] np = {r.cartesian(0), r.cartesian(1)};
+        tail.set(149, np);*/
+    }
 
     public Vector forceFrom(Body b) {
         Body a = this;
@@ -56,13 +71,20 @@ public class Body {
         return delta.direction().times(F);
     } // forceFrom( Body )
 
-    /** makes the dots appear with the desired size and color
+    /**
+     * makes the dots appear with the desired size and color
      *
      */
     public void draw() {
         StdDraw.setPenRadius(size); //changes size
         StdDraw.setPenColor(color); //changes the color
         StdDraw.point(r.cartesian(0), r.cartesian(1));
+        for (int i = 0; i < tail.size() - 1; i++) {
+            double[] first = tail.get(i);
+            double[] second = tail.get(i + 1);
+            StdDraw.setPenRadius(size*0.5);
+            StdDraw.line(first[0], first[1], second[0], second[1]);
+        }
     } // draw()
     // GIT IS WORKING
     // this method is only needed if you want to change the size of the bodies
@@ -70,6 +92,7 @@ public class Body {
     public void draw(double penRadius) {
         StdDraw.setPenRadius(penRadius);
         StdDraw.point(r.cartesian(0), r.cartesian(1));
+
     } // draw( double )
 
 } // Body
